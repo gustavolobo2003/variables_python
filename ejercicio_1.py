a46b494d56014d8188c65041d42bc99d5d404a5e
gustavolobo2003 <gustavolobo2003@gmail.com>
2021-09-17 22:48:41 -0300

    Add files via upload

---
+++ ejercicio_1.py
@@ -0,0 +1,35 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica
+
+numero_1 = 5
+numero_2 = 7
+
+suma = numero_1 + numero_2 
+print (" el valor de numero es ", numero_1)
+
+print (" el valor de numero es ", numero_2)
+
+print ("el resultado de la suma es", suma)
+
+resta = numero_1 - numero_2 
+
+print ("el resultado de restar", numero_1, "y", numero_2, "y", resta)
+
+# Realizar la suma de las dos variables
+# numero_1 y numero_2
+# Almacenar el valor de la suma en una variable
+# ej:
+# operacion = .....
+
+# Imprimir en pantalla el resultado de la suma
+# print(....)
+
+# Repita el procedimiento para realizar la resta
---
+++ ejercicio_2.py
@@ -0,0 +1,53 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica y consola
+
+# Ahora los valores a operar deben ser ingresados por
+# consola con la función "input" como se ve a continuación
+print('Ingrese por consola el primer número decimal a operar:')
+numero_1 = int(input())
+print ("numero ingresado:", numero_1)
+
+print('Ingrese por consola el segundo número decimal a operar:')
+numero_2 = int(input())
+print ("numero ingresado:", numero_2 )
+
+print (" 4 + 2=" )
+numero_3 = int(input())
+print ("el resultado de la suma es:", numero_3)
+
+print ("4 - 2 =")
+numero_4 = int(input())
+print ("el resultado de la resta es:", numero_4)
+
+print ("4 * 2 =")
+numero_5 = int(input())
+print ("el resultado de la multiplicacion es:", numero_5)
+
+print ("4 / 2 =")
+numero_6 = int(input())
+print ("el resultado de la divicion es:", numero_6)
+
+# Alumno: Imprima en pantalla los dos números decimales solicitados
+# print(....)
+
+# Alumno: Calcule la suma, resta, división y multiplicación de los números ingresados
+# numero_1, numero_2
+# Imprima en pantalla todos los resultados con el siguiente formato de ejemplo:
+# El resultado de sumar 4 y 2 es 6
+# NOTA: No coloque usted los nùmeros y resultados, use las variables
+
+# Suma
+
+# Resta
+
+# División
+
+# Multiplicación
